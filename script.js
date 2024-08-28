225fc3fc0309947df18766180cdcb72a48aedeb9
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-08-28 08:56:48 +0200

    Import johannes, nwabisa, and alex from their modules

---
+++ script.js
@@ -0,0 +1,6 @@
+// scripts.js
+import { johannes } from './johannes.js';
+import { nwabisa } from './nwabisa.js';
+import { alex } from './alex.js';
+
+console.log('Roles:', nwabisa.role, johannes.role, alex.role)
