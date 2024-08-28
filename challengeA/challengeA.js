4adad986ff0452af5f882ed3a8c8e9b7b9c36d14
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-08-28 11:13:10 +0200

    added challenges A - B

---
+++ challengeA/challengeA.js
@@ -0,0 +1,15 @@
+// Challenge A
+
+/* This is the firstname of the user
+    const user = 'John'
+
+/* This is the lastname of the user /* 
+    const surname = 'Smith'
+
+    console.log(user; surname;)
+*/
+
+const user = 'John';
+const surname = 'Smith';
+
+console.log(user, surname);
---
+++ challengeC/challengeC.js
@@ -0,0 +1,18 @@
+// Challenge C
+/*
+// 
+It is important to show the following message in order to:
+- to assure users
+- to scare hackers
+- to impress investors
+//
+
+console.warn(Security scan starting)
+
+/*
+ It is important to let user know when they can close the page
+cnosole.info('Please wait for scan to complete before closing the browser.)
+*/
+
+console.warn('Security scan starting');
+console.info('Please wait for scan to complete before closing the browser')
