a3504ab8c26489a768cd2ebaad4435b50851ae18
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-08-28 08:55:09 +0200

    Export nwabisa object and update DOM

---
+++ nwabisa.js
@@ -0,0 +1,12 @@
+
+// nwabisa.js
+
+export const nwabisa = {
+    firstname: "Nwabisa",
+    surname: "Gabe",
+    role: "CEO"
+};
+
+ display= nwabisa.firstname + " " + nwabisa.surname + " (" + nwabisa.role + ")"
+ document.querySelector('#nwabisa').innerText = display
+
