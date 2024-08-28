8f9f5519c84192c5b25218b3b516d36f30f88a39
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-08-28 11:33:00 +0200

    added comments, changed declarations and fixed parenthesis

---
+++ challengeF/challengeF.js
@@ -0,0 +1,38 @@
+// changed declaration from "const" to "let"
+let date = 2050;
+let userStatus = "student"; // changed variable name from "status" to "userStatus"
+let count = 0;
+
+// added strict equal operator
+// removed "let" declarations
+if (date === 2050) {
+	console.log("January", 'New Year’s Day');
+	console.log("March", 'Human Rights Day');
+	date = 'April';
+	console.log(date, 'Family Day');
+	console.log(date, 'Freedom Day');
+	 count = count + 4
+
+	if (userStatus === "student") {
+	  console.log('June', 'Youth Day')
+		 count = count + 1
+  }
+
+	console.log('August', 'Women’s Day')
+	console.log('September', 'Heritage Day')
+	date = 'December'
+	console.log(date, 'Day of Reconciliation')
+	 count = count + 3
+
+	if (userStatus = "parent") {
+	  console.log(date, 'Christmas Day')
+		 count = count + 1
+  }
+
+	console.log(date, 'Day of Goodwill')
+	 count = count + 1
+}
+
+console.log('Your status is:', userStatus)
+console.log('The year is:', date)
+console.log('The total holidays is:', count)
