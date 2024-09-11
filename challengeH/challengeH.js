3258b3259d0602046efa3fa9ed665efa971a8aa7
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-09-11 11:42:12 +0200

    completed challengeH

---
+++ challengeH/challengeH.js
@@ -0,0 +1,34 @@
+const primaryPhone = 'O748105141';
+const secondaryPhone = '0219131568';
+
+// Only change below this line
+
+function checkForValidPhoneNumber(phone) {
+
+    // checks if phone is not a string
+    if (typeof phone !== 'string') {
+        return false;
+    }
+
+    /*
+     loop continues as long as i is the length
+     of the phone string.
+     after each iteration, increase by 1
+    */
+    for (let i = 0; i < phone.length; i++) {
+        if (phone[i] < '0' || phone[i] > '9') {
+            return false;
+         }
+    }
+    return true;
+}
+
+/* 
+ primaryPhone & secondaryPhone is the placeholder values
+ for the checkForValidPhoneNumber function
+*/
+const primaryValid = checkForValidPhoneNumber(primaryPhone);
+const secondaryValid = checkForValidPhoneNumber(secondaryPhone)
+
+console.log('Primary phone is valid numerical string:', primaryValid);
+console.log('Secondary phone is valid numerical string:', secondaryValid );
