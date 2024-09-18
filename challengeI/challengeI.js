d115612a13571b01a3e1af2a13cfe31959bf9836
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-09-18 12:06:32 +0200

    added event listener

--- challengeI/challengeI.js
+++ challengeI/challengeI.js
@@ -3,15 +3,16 @@ const tax = '8%';
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
-const hourOfDay = 00;
-const minuteOfDay = 00;
+const hourOfDay = 0;
+const minuteOfDay = 0;
 
 // Only change below this line
 
-if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
-	const taxAsDecimal = tax / '100'
-  const startingAfterTax = salary * '1' - taxAsDecimal
-	const balace = starting - transport - food - rent
-}
-	
-console.log(balance.toFixed(3))
+	document.getElementById('calculateBtn').addEventListener('click', function () {
+	if (hourOfDay === 0 && minuteOfDay === 0) {
+	const taxAsDecimal = parseFloat(tax.replace('%', '')) / 100;
+	const startingAfterTax = salary * (1 - taxAsDecimal);
+	const balance = startingAfterTax - transport - food - rent;
+	document.getElementById('balance').innerText = '$' + balance.toFixed(2);
+	}
+});	
